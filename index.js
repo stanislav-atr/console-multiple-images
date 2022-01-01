07960cb0422e0ced2e96265bade97c16c58ffc7a
Stanislav A <s.atroschenko@adguard.com>
2022-01-01 22:09:16 +0300

    handle image not loading

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ win.console.images = consoleImages;
 const testUrls = [
     'https://cdnpt01.viewbug.com/media/mediafiles/2016/01/11/62182159_large1300.jpg',
     'https://cdn.pixabay.com/photo/2015/11/29/13/08/kingfisher-1068684_1280.jpg',
+    'noImage',
     'https://cdn.pixabay.com/photo/2019/02/06/17/09/snake-3979601_1280.jpg',
 ];
 consoleImages(testUrls);
